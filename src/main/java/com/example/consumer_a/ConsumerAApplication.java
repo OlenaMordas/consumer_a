fbe65f5b26382426bd7e8b1fc147d68044b28902
olena.mordas <olena.mordas@synpulse.com>
2021-05-05 11:22:32 +0200

    Initial commit

---
+++ src/main/java/com/example/consumer_a/ConsumerAApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumer_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerAApplication.class, args);
+	}
+
+}
