5bd8b39d1e467d5d0eaa0fc190aef9181e2265dd
olena.mordas <olena.mordas@synpulse.com>
2021-05-07 14:39:41 +0200

    check if entities are in db

--- src/test/java/com/example/consumer_a/consumer/EventsConsumerIntgTest.java
+++ src/test/java/com/example/consumer_a/consumer/EventsConsumerIntgTest.java
@@ -1,5 +1,11 @@
 package com.example.consumer_a.consumer;
 
+import com.example.consumer_a.entity.Account;
+import com.example.consumer_a.entity.Client;
+import com.example.consumer_a.entity.Transaction;
+import com.example.consumer_a.repository.AccountRepository;
+import com.example.consumer_a.repository.ClientRepository;
+import com.example.consumer_a.repository.TransactionRepository;
 import com.example.consumer_a.service.AccountService;
 import com.example.consumer_a.service.ClientService;
 import com.example.consumer_a.service.TransactionService;
@@ -18,6 +24,7 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -49,6 +56,13 @@ public class EventsConsumerIntgTest {
     @SpyBean
     TransactionService transactionServiceSpy;
 
+    @Autowired
+    ClientRepository clientRepository;
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    TransactionRepository transactionRepository;
+
     @BeforeEach
     void setUp(){
         //make sure that container got all the partitions assigned
@@ -61,13 +75,13 @@ public class EventsConsumerIntgTest {
     void newEvent() throws ExecutionException, InterruptedException, JsonProcessingException {
 
         //given
-        String jsonClient ="{client_id: 66123123123, first_name: John, last_name: Doe, ebanking_id: EB0001234}";
+        String jsonClient ="{client_id: 00000000000, first_name: Test, last_name: Test, ebanking_id: EB0000000}";
         kafkaTemplate.send("clients", jsonClient).get();
 
-        String jsonAccount = "{'client_id': '66456456789', 'account_nr': '31926835', 'account_type': 'Cash', 'ccy': 'USD', 'iban': 'GB95NWBK70011931926835', 'balance': '37273'}";
+        String jsonAccount = "{'client_id': '00000000000', 'account_nr': '00000000', 'account_type': 'Cash', 'ccy': 'USD', 'iban': 'GB95NWBK70011931900000', 'balance': '10000'}";
         kafkaTemplate.send("accounts", jsonAccount).get();
 
-        String jsonTransaction = "{transaction_id: 81d3a179-b1bc-465b-a9ee-000000053182, amount: \"CHF -3020.27\", iban: GB22NWBK70011931926888, value_date: \"2015-18-08 11:33:05\", description: \"Centennial Bank WITHDRAW\", account_id: 31926834}";
+        String jsonTransaction = "{transaction_id: 81d3a179-b1bc-465b-a9ee-000000000000, amount: \"CHF -3020.27\", iban: GB95NWBK70011931900000, value_date: \"2020-18-08 11:33:05\", description: \"Centennial Bank WITHDRAW\", account_id: 00000000}";
         kafkaTemplate.send("transactions", jsonTransaction).get();
 
         //when
@@ -79,5 +93,14 @@ public class EventsConsumerIntgTest {
         verify(clientServiceSpy, times(1)).processRecordValue(isA(ConsumerRecord.class));
         verify(accountServiceSpy, times(1)).processRecordValue(isA(ConsumerRecord.class));
         verify(transactionServiceSpy, times(1)).processRecordValue(isA(ConsumerRecord.class));
+
+        Optional<Client> clientOptional = clientRepository.findById("00000000000");
+        assert clientOptional.isPresent();
+
+        Optional <Account> accountOptional = accountRepository.findById("00000000");
+        assert accountOptional.isPresent();
+
+        Optional <Transaction> transactionOptional = transactionRepository.findById("81d3a179-b1bc-465b-a9ee-000000000000");
+        assert transactionOptional.isPresent();
     }
 }
